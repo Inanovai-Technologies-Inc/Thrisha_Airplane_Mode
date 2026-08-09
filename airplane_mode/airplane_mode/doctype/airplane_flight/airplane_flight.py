9ffb506c163a6808774c19c0cdb4651800d693b2
Thrisha-Shetty1624 <thrisha.shetty@inanovai.com>
2026-08-09 15:00:56 +0000

    Fixed changes

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -4,8 +4,6 @@ from frappe.website.website_generator import WebsiteGenerator
 class AirplaneFlight(WebsiteGenerator):
 
     def on_submit(self):
-
         self.status = "Completed"
-
         self.db_set("status", "Completed")
         
