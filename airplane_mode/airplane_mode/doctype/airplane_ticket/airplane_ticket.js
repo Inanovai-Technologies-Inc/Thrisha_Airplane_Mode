21011f54b2c9efeab87b16e5ebda588b2f1d7651
Akshitha N R <akshithanr481@gmail.com>
2026-08-11 14:26:02 +0000

    style: apply CI formatting

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,26 +1,26 @@
 frappe.ui.form.on("Airplane Ticket", {
-    refresh(frm) {
-        frm.add_custom_button(__("Assign Seat"), () => {
-            let dialog = new frappe.ui.Dialog({
-                title: __("Assign Seat"),
+	refresh(frm) {
+		frm.add_custom_button(__("Assign Seat"), () => {
+			let dialog = new frappe.ui.Dialog({
+				title: __("Assign Seat"),
 
-                fields: [
-                    {
-                        label: __("Seat"),
-                        fieldname: "seat",
-                        fieldtype: "Data",
-                    },
-                ],
+				fields: [
+					{
+						label: __("Seat"),
+						fieldname: "seat",
+						fieldtype: "Data",
+					},
+				],
 
-                primary_action_label: __("Assign"),
+				primary_action_label: __("Assign"),
 
-                primary_action(values) {
-                    frm.set_value("seat", values.seat);
-                    dialog.hide();
-                },
-            });
+				primary_action(values) {
+					frm.set_value("seat", values.seat);
+					dialog.hide();
+				},
+			});
 
-            dialog.show();
-        });
-    },
+			dialog.show();
+		});
+	},
 });
