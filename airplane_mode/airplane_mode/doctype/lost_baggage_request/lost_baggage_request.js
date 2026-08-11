3d483182a2f643548d6ee2de6c55a45ec827ff91
Akshitha N R <akshithanr481@gmail.com>
2026-08-11 18:35:39 +0000

    fix: wrap lost baggage messages for linter

--- airplane_mode/airplane_mode/doctype/lost_baggage_request/lost_baggage_request.js
+++ airplane_mode/airplane_mode/doctype/lost_baggage_request/lost_baggage_request.js
@@ -9,14 +9,14 @@ frappe.ui.form.on("Lost Baggage Request", {
 	delivery_status(frm) {
 		if (frm.doc.delivery_status === "Pending") {
 			frappe.msgprint({
-				title: "Delivery Pending",
-				message: "The lost baggage is not yet ready for delivery.",
+				title: __("Delivery Pending"),
+				message: __("The lost baggage is not yet ready for delivery."),
 				indicator: "orange",
 			});
 		} else if (frm.doc.delivery_status === "Ready for Delivery") {
 			frappe.msgprint({
-				title: "Ready for Delivery",
-				message: "The lost baggage is ready to be delivered to the customer.",
+				title: __("Ready for Delivery"),
+				message: __("The lost baggage is ready to be delivered to the customer."),
 				indicator: "blue",
 			});
 
@@ -27,17 +27,17 @@ frappe.ui.form.on("Lost Baggage Request", {
 
 function add_delivery_button(frm) {
 	if (!frm.is_new() && frm.doc.delivery_status === "Ready for Delivery") {
-		frm.add_custom_button("Deliver Lost Baggage", () => {
+		frm.add_custom_button(__("Deliver Lost Baggage"), () => {
 			frappe.confirm(
-				"Are you sure you want to mark this baggage as delivered to the customer?",
+				__("Are you sure you want to mark this baggage as delivered to the customer?"),
 				() => {
 					frm.set_value("delivery_status", "Delivered");
 					frm.set_value("delivered_on", frappe.datetime.now_datetime());
 
 					frm.save().then(() => {
 						frappe.msgprint({
-							title: "Success",
-							message: "Lost baggage delivered successfully.",
+							title: __("Success"),
+							message: __("Lost baggage delivered successfully."),
 							indicator: "green",
 						});
 					});
