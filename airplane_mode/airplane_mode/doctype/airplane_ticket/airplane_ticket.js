131ae74823bb7b3b673a7558bf93436969f92125
Akshitha N R <akshithanr481@gmail.com>
2026-08-11 14:13:13 +0000

    fix: resolve CI linter and Frappe v15 issues

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,27 +1,26 @@
 frappe.ui.form.on("Airplane Ticket", {
-	refresh(frm) {
-		frm.add_custom_button("Assign Seat", () => {
-			let dialog = new frappe.ui.Dialog({
-				title: "Assign Seat",
+    refresh(frm) {
+        frm.add_custom_button(__("Assign Seat"), () => {
+            let dialog = new frappe.ui.Dialog({
+                title: __("Assign Seat"),
 
-				fields: [
-					{
-						label: "Seat",
-						fieldname: "seat",
-						fieldtype: "Data",
-					},
-				],
+                fields: [
+                    {
+                        label: __("Seat"),
+                        fieldname: "seat",
+                        fieldtype: "Data",
+                    },
+                ],
 
-				primary_action_label: "Assign",
+                primary_action_label: __("Assign"),
 
-				primary_action(values) {
-					frm.set_value("seat", values.seat);
+                primary_action(values) {
+                    frm.set_value("seat", values.seat);
+                    dialog.hide();
+                },
+            });
 
-					dialog.hide();
-				},
-			});
-
-			dialog.show();
-		});
-	},
+            dialog.show();
+        });
+    },
 });
--- airplane_mode/airport_shop/doctype/shop/shop.py
+++ airplane_mode/airport_shop/doctype/shop/shop.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2026, Thrisha and contributors
+
 # For license information, please see license.txt
 
 import frappe
@@ -6,29 +7,39 @@ from frappe.model.document import Document
 
 
 class Shop(Document):
-	def before_insert(self):
-		if not self.rent_amount:
-			default_rent = frappe.db.get_single_value("Airport Shop Settings", "default_rent_amount")
-			self.rent_amount = default_rent
+    def before_insert(self):
+        if not self.rent_amount:
+            default_rent = frappe.db.get_single_value(
+                "Airport Shop Settings", "default_rent_amount"
+            )
+            self.rent_amount = default_rent
 
-	def after_save(self):
-		self.update_airport_counts()
+    def on_update(self):
+        self.update_airport_counts()
 
-	def on_trash(self):
-		self.update_airport_counts()
+    def on_trash(self):
+        self.update_airport_counts()
 
-	def update_airport_counts(self):
-		if not self.airport:
-			return
+    def update_airport_counts(self):
+        if not self.airport:
+            return
 
-		total = frappe.db.count("Shop", {"airport": self.airport})
+        total = frappe.db.count("Shop", {"airport": self.airport})
 
-		occupied = frappe.db.count("Shop", {"airport": self.airport, "status": "Occupied"})
+        occupied = frappe.db.count(
+            "Shop", {"airport": self.airport, "status": "Occupied"}
+        )
 
-		available = frappe.db.count("Shop", {"airport": self.airport, "status": "Available"})
+        available = frappe.db.count(
+            "Shop", {"airport": self.airport, "status": "Available"}
+        )
 
-		frappe.db.set_value(
-			"Airport",
-			self.airport,
-			{"total_shops": total, "occupied_shops": occupied, "available_shops": available},
-		)
+        frappe.db.set_value(
+            "Airport",
+            self.airport,
+            {
+                "total_shops": total,
+                "occupied_shops": occupied,
+                "available_shops": available,
+            },
+        )
