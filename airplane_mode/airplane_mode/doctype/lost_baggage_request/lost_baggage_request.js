ecaae628c96e1c3fce0654f7f2855fdf6f190812
Akshitha N R <akshithanr481@gmail.com>
2026-08-12 04:19:21 +0000

    feat: add delivery preference and address for lost baggage

--- airplane_mode/airplane_mode/doctype/lost_baggage_request/lost_baggage_request.js
+++ airplane_mode/airplane_mode/doctype/lost_baggage_request/lost_baggage_request.js
@@ -4,6 +4,29 @@
 frappe.ui.form.on("Lost Baggage Request", {
 	refresh(frm) {
 		add_delivery_button(frm);
+		set_delivery_address_visibility(frm);
+	},
+
+	customer(frm) {
+		if (frm.doc.customer) {
+			frappe.db.get_value("Customer", frm.doc.customer, "customer_primary_address", (r) => {
+				if (r && r.customer_primary_address) {
+					frm.set_value("customer_address", r.customer_primary_address);
+				} else {
+					frm.set_value("customer_address", "");
+				}
+			});
+		} else {
+			frm.set_value("customer_address", "");
+		}
+	},
+
+	delivery_preference(frm) {
+		set_delivery_address_visibility(frm);
+	},
+
+	use_different_address(frm) {
+		set_delivery_address_visibility(frm);
 	},
 
 	delivery_status(frm) {
@@ -24,6 +47,17 @@ frappe.ui.form.on("Lost Baggage Request", {
 		}
 	},
 });
+function set_delivery_address_visibility(frm) {
+	const is_airport_delivery = frm.doc.delivery_preference === "Airport Delivery";
+
+	// Show checkbox only for Airport Delivery
+	frm.toggle_display("use_different_address", is_airport_delivery);
+
+	// Show delivery address only when checkbox is checked
+	const show_delivery_address = is_airport_delivery && frm.doc.use_different_address === 1;
+
+	frm.toggle_display("delivery_address", show_delivery_address);
+}
 
 function add_delivery_button(frm) {
 	if (!frm.is_new() && frm.doc.delivery_status === "Ready for Delivery") {
