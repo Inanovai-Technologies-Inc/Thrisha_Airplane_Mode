2d789a05e07cb2ba77fdcecf9b16c45cca9769dc
Akshitha N R <akshithanr481@gmail.com>
2026-08-13 05:54:05 +0000

    fix: send email when request status changes

--- airplane_mode/airplane_mode/doctype/lost_baggage_request/lost_baggage_request.py
+++ airplane_mode/airplane_mode/doctype/lost_baggage_request/lost_baggage_request.py
@@ -7,18 +7,23 @@ from frappe.model.document import Document
 
 
 class LostBaggageRequest(Document):
-	def on_update(self):
-		if self.has_value_changed("delivery_status"):
-			self.send_status_update_email()
+    def on_update(self):
+        if self.has_value_changed("status"):
+            self.send_status_update_email()
 
-	def send_status_update_email(self):
-		if self.email:
-			template = frappe.get_doc("Email Template", "Lost Baggage Status Update")
+    def send_status_update_email(self):
+        if self.email:
+            template = frappe.get_doc(
+                "Email Template", "Lost Baggage Status Update"
+            )
 
-			message = frappe.get_template(template.response_html).render(self.as_dict())
+            message = frappe.render_template(
+                template.response_html,
+                self.as_dict(),
+            )
 
-			frappe.sendmail(
-				recipients=[self.email],
-				subject=template.subject,
-				message=message,
-			)
+            frappe.sendmail(
+                recipients=[self.email],
+                subject=template.subject,
+                message=message,
+            )
