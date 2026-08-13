3c4c2f7a5c2cc36aa7e7fd9b94dba40ec371e5b2
Thrisha-Shetty1624 <thrisha.shetty@inanovai.com>
2026-08-13 08:31:05 +0000

    fixed code

--- airplane_mode/public/js/airplane_mode_desk_component.js
+++ airplane_mode/public/js/airplane_mode_desk_component.js
@@ -1,7 +1,7 @@
 // Minimal placeholder for Airplane Mode desk component
 // This file intentionally left minimal to avoid asset 404s.
 (function () {
-    if (typeof console !== 'undefined') {
-        console.log('airplane_mode desk component loaded');
-    }
+	if (typeof console !== "undefined") {
+		console.log("airplane_mode desk component loaded");
+	}
 })();
