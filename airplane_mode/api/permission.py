08fdc171511889f06d22e0b36ad1938234b11386
Akshitha N R <akshithanr481@gmail.com>
2026-08-13 04:27:42 +0000

    fix: format app permission

--- airplane_mode/api/permission.py
+++ airplane_mode/api/permission.py
@@ -2,12 +2,12 @@ import frappe
 
 
 def has_app_permission():
-    allowed_roles = {
-        "System Manager",
-        "Airport Authority Personnel",
-        "Fleet Manager",
-        "Flight Crew Member",
-        "Travel Agent",
-    }
+	allowed_roles = {
+		"System Manager",
+		"Airport Authority Personnel",
+		"Fleet Manager",
+		"Flight Crew Member",
+		"Travel Agent",
+	}
 
-    return bool(set(frappe.get_roles()) & allowed_roles)
+	return bool(set(frappe.get_roles()) & allowed_roles)
